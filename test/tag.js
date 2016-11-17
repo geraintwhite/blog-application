7532d606c963f0672a33a8813014c61f74856ca9
Geraint White <geraintwhite@gmail.com>
2016-11-17 20:06:59 +0000

    add getAll tags tests

--- test/tag.js
+++ test/tag.js
@@ -5,17 +5,53 @@ const tests = (t) => {
   t.test('tag.getAll error', (st) => {
     const db = {}, api = new TagAPI(db);
 
-    st.plan(4);
+    st.plan(3);
 
     db.getTags = (cb) => {
-      st.pass('db.getTags should be called');
+      st.pass('db.getTags is called');
       cb(true, null);
-    }
+    };
 
     api.getAll((code, data) => {
-      st.equal(code, 500, 'should return correct status code');
-      st.ok(data);
-      st.equal(data.err, 'Server error', 'should return correct error message');
+      st.equal(code, 500, 'correct status code');
+      st.equal(data.err, 'Server error', 'correct error message');
+      st.end();
+    });
+  });
+
+  t.test('tag.getAll no tags', (st) => {
+    const db = {}, api = new TagAPI(db);
+
+    st.plan(3);
+
+    db.getTags = (cb) => {
+      st.pass('db.getTags is called');
+      cb(false, []);
+    };
+
+    api.getAll((code, data) => {
+      st.equal(code, 200, 'correct status code');
+      st.deepEqual(data.tags, [], 'empty tags array');
+      st.end();
+    });
+  });
+
+  t.test('tag.getAll success', (st) => {
+    const db = {}, api = new TagAPI(db);
+
+    const tags = [{id: 1, name: 'tag1'},
+                  {id: 2, name: 'tag2'}];
+
+    st.plan(3);
+
+    db.getTags = (cb) => {
+      st.pass('db.getTags is called');
+      cb(false, tags);
+    };
+
+    api.getAll((code, data) => {
+      st.equal(code, 200, 'correct status code');
+      st.deepEqual(data.tags, tags, 'correct tags array');
       st.end();
     });
   });
