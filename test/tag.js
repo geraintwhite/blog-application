05e7266958d07c1a3188966779646ee688d1b155
Geraint White <geraintwhite@gmail.com>
2016-11-21 20:00:28 +0000

    createTag

--- test/tag.js
+++ test/tag.js
@@ -129,6 +129,102 @@ const tests = (t) => {
     });
   });
 
+  t.test('tag.create error', (st) => {
+    const db = {}, api = new TagAPI(db);
+
+    const tag = {name: 'tag1'};
+
+    st.plan(5);
+
+    db.getTags = (cb) => {
+      st.pass('db.getTags is called');
+      cb(null, []);
+    };
+
+    db.createTag = (obj, cb) => {
+      st.pass('db.createTag is called');
+      st.deepEqual(obj, tag, 'correct object passed in');
+      cb(true, null);
+    };
+
+    api.create(tag, (code, data) => {
+      st.equal(code, 500, 'correct status code');
+      st.equal(data.err, 'Server error', 'correct error message');
+      st.end();
+    });
+  });
+
+  t.test('tag.create getTags error', (st) => {
+    const db = {}, api = new TagAPI(db);
+
+    const tag = {name: 'tag1'};
+
+    st.plan(3);
+
+    db.getTags = (cb) => {
+      st.pass('db.getTags is called');
+      cb(true, null);
+    };
+
+    db.createTag = (obj, cb) => {
+      st.fail('db.createTag should not be called');
+      st.end();
+    };
+
+    api.create(tag, (code, data) => {
+      st.equal(code, 500, 'correct status code');
+      st.equal(data.err, 'Server error', 'correct error message');
+      st.end();
+    });
+  });
+
+  t.test('tag.create tag already exists', (st) => {
+    const db = {}, api = new TagAPI(db);
+
+    const tag = {name: 'tag1'};
+
+    st.plan(3);
+
+    db.getTags = (cb) => {
+      st.pass('db.getTags is called');
+      cb(null, [tag]);
+    };
+
+    db.createTag = (obj, cb) => {
+      st.fail('db.createTag should not be called');
+      st.end();
+    };
+
+    api.create(tag, (code, data) => {
+      st.equal(code, 400, 'correct status code');
+      st.equal(data.err, 'Tag already exists', 'correct error message');
+      st.end();
+    });
+  });
+
+  t.test('tag.create validation fails', (st) => {
+    const db = {}, api = new TagAPI(db);
+
+    const tag = {};
+
+    st.plan(2);
+
+    db.getTags = (cb) => {
+      st.fail('db.getTags should not be called');
+      st.end();
+    };
+
+    db.createTag = (obj, cb) => {
+      st.fail('db.createTag should not be called');
+      st.end();
+    };
+
+    api.create(tag, (code, data) => {
+      st.equal(code, 400, 'correct status code');
+      st.equal(data.err, 'Invalid tag name', 'correct error message');
+      st.end();
+    });
+  });
 };
 
 
