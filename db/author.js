c4c0265336f85e793fa975c0ba0a6797de8a2417
Geraint White <geraintwhite@gmail.com>
2016-11-24 15:25:37 +0000

    add articles by author page

--- db/author.js
+++ db/author.js
@@ -1,6 +1,33 @@
+import {articleSelect, groupTags} from './article';
+
+
 class AuthorDB {
   constructor(pool) {
     this.pool = pool;
+  }
+
+  getAuthor(author_id, cb) {
+    const sql =
+      'SELECT user_id, name, email ' +
+      'FROM user ' +
+      'WHERE user_id = ? ' +
+      'AND is_author';
+
+    this.pool.query(sql, [author_id], (err, rows) => {
+      if (err) console.error(err);
+      cb(err, rows && rows[0]);
+    });
+  }
+
+  getArticlesByAuthor(author_id, cb) {
+    const sql = articleSelect +
+      'WHERE article.author_id = ? ' +
+      'GROUP BY article.article_id';
+
+    this.pool.query(sql, [author_id], (err, rows) => {
+      if (err) console.error(err);
+      cb(err, rows && groupTags(rows));
+    });
   }
 }
 
--- routes/author.js
+++ routes/author.js
@@ -8,4 +8,23 @@ const authorAPI = new AuthorAPI(new AuthorDB(pool));
 const router = express.Router();
 
 
+router.get('/author/:id', (req, res) => {
+  authorAPI.get(req.params.id, (code, data) => {
+    if (code !== 200) {
+      return res.render('error', {title: 'Error', message: data.err});
+    }
+
+    const author = data.author;
+
+    authorAPI.getArticles(req.params.id, (code, data) => {
+      if (code !== 200) {
+        res.render('error', {title: 'Error', message: data.err});
+      } else {
+        res.render('articles', {title: author.name, articles: data.articles});
+      }
+    });
+  });
+});
+
+
 export default router;
